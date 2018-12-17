55457e7ce75f73ffa051f2503b2b7cc431c8336e
Khazbs <khazbartur@yandex.ru>
2018-12-17 21:40:16 +0300

    Add editor

--- static/editor.js
+++ static/editor.js
@@ -10,6 +10,12 @@ function fetchSchedule() {
 		if (this.readyState == 4 && this.status == 200) {
 			var week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 			var startGrade = 5;
+			var subjectOptions = '';
+			for (var key in subjects) {
+				if (subjects.hasOwnProperty(key)) {
+					subjectOptions += '<option value="' + key + '">' + subjects[key] + '</option>';
+				}
+			}
 			var scheduleElement = document.getElementById('schedule');
 			schedule = JSON.parse(scheduleXhttpRequest.responseText);
 			for (var i = 1; i <= schedule.length; ++i) {  // Iterate over weekdays
@@ -21,10 +27,17 @@ function fetchSchedule() {
 					var gradeElement = document.getElementById('grade-' + (j + startGrade) + '-' + i);
 					for (var k = 0; k < schedule[i - 1][j].length; ++k) {  // Iterate over lessons
 						gradeElement.innerHTML += '<tr id="lesson-' + (j + startGrade) + '-' + (k + 1) + '-' + i + '"><td>' + (k + 1) +
-							'</td><td>' + (subjects[schedule[i - 1][j][k]] || schedule[i - 1][j][k]) + '</td></tr>';
+							'</td><td>' + (schedule[i - 1][j][k] != '' ? '<select id="select-' + (j + startGrade) + '-' + (k + 1) + '-' + i + '">' + subjectOptions + '</select>' : '') + '</td></tr>';
 					};
 				};
 			};
+			for (var i = 1; i <= schedule.length; ++i) {
+				for (var j = 0; j < schedule[i - 1].length; ++j) {
+					for (var k = 0; k < schedule[i - 1][j].length; ++k) {
+						document.getElementById('select-' + (j + startGrade) + '-' + (k + 1) + '-' + i).value = schedule[i - 1][j][k];
+					}
+				}
+			}
 		}
 	};
 	subjectsXhttpRequest.onreadystatechange = function() {
